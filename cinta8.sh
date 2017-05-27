6463e0e2185109efd4b23b35107f894fdf4ca251
elangoverdosis <deeniedoank@gmail.com>
2017-05-27 06:03:41 +0000

    Update and rename debian8.sh to cinta8.sh

---
+++ cinta8.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+if [[ $USER != "root" ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+#MYIP=$(wget -qO- ipv4.icanhazip.com);
+
+# get the VPS IP
+#ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+
+#MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
+if [[ $ether = "" ]]; then
+        ether=eth0
+fi
+
+#vps="zvur";
+vps="aneka";
+
+#if [[ $vps = "zvur" ]]; then
+	#source="http://"
+#else
+	source="https://raw.githubusercontent.com/elangoverdosis/deeniedoank2"
+#fi
+
+# go to root
+cd
+# pass
+clear
+read -p "Silahkan masukkan password installer script: " passwds
+wget -q -O /usr/bin/pass $source/debian8/password.txt
+if ! grep -w -q $passwds /usr/bin/pass; then
+clear
+echo " Maaf, PASSWORD salah silahkan hubungi admin"
+rm /usr/bin/pass
+rm cinta8.sh
+
+exit
+fi
+# check registered ip
+wget -q -O IP $source/debian8/IP.txt
+if ! grep -w -q $MYIP IP; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+        echo "     
+                       
+               =============== OS-32 & 64-bit ================
+               ♦                                             ♦
+               ♦   AUTOSCRIPT CREATED BY YUSUF ARDIANSYAH    ♦
+	             ♦                     &                       ♦
+	             ♦               DENY SISWANTO                 ♦
+               ♦       -----------About Us------------       ♦ 
+               ♦            Tel : +6283843700098             ♦
+               ♦         { Sms/whatsapp/telegram }           ♦ 
+               ♦       http://facebook.com/t34mh4ck3r        ♦    
+               ♦   http://www.facebook.com/elang.overdosis   ♦
+               ♦                                             ♦
+               =============== OS-32 & 64-bit ================
+               
+                 Please make payment before use auto script
+                 ..........................................
+                 .        Price: Rp.20.000 = 1IP          .
+                 .          *****************             .
+                 .           Maybank Account              .
+                 .           =================            .
+                 .          No   : Hubungi admin          .
+                 .          Name : Yusuf Ardiansyah       .
+                 ..........................................   
+                          Thank You For Choice Us"
+
+	echo "        Hubungi: editor ( elang overdosis atau deeniedoank)"
+	
+	rm /root/IP
+	rm cinta8.sh
+	rm -f /root/IP
+	exit
+fi
+
+# go to root
+cd
+
+# Install Command
+apt-get -y install ufw
+apt-get -y install sudo
+
+# Install Pritunl
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv EA312927
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install pritunl mongodb-org
+systemctl start mongod pritunl
+systemctl enable mongod pritunl
+
+# Install Squid
+apt-get -y install squid3
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+wget -O /etc/squid3/squid.conf $source/debian8/conf/squid.conf 
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
+service squid3 restart
+
+# Enable Firewall
+sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
+sudo ufw allow 22,80,81,222,443,8080,9700,60000/udp
+sudo yes | ufw enable
+
+# Change to Time GMT+8
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# Install Web Server
+apt-get -y install nginx php5-fpm php5-cli
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf $source/debian8/conf/nginx.conf
+mkdir -p /home/vps/public_html
+echo "<pre>Setup by Yusuf Ardiansyah</pre>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+wget -O /etc/nginx/conf.d/vps.conf $source/debian8/conf/vps.conf
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+service nginx restart
+
+# Install Vnstat
+apt-get -y install vnstat
+vnstat -u -i eth0
+sudo chown -R vnstat:vnstat /var/lib/vnstat
+service vnstat restart
+
+# Install Vnstat GUI
+cd /home/vps/public_html/
+wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
+
+# About
+clear
+echo "Script ini hanya mengandungi :-"
+echo "-Pritunl"
+echo "-MongoDB"
+echo "-Vnstat"
+echo "-Web Server"
+echo "-Squid Proxy Port 7166,60000"
+echo "BY MKSSHVPN"
+echo "TimeZone   :  Malaysia"
+echo "Vnstat     :  http://$MYIP:81/vnstat"
+echo "Pritunl    :  https://$MYIP"
+echo "Sila login ke pritunl untuk proceed step seterusnya"
+echo "Sila copy code dibawah untuk Pritunl anda"
+pritunl setup-key
